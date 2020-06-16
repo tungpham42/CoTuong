0f7e0daa953e6329322bc55df31d4c70effb1f4d
Tung Pham <tung.42@gmail.com>
2020-06-16 09:32:14 +0700

    mail

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -14,22 +14,22 @@ class MailController extends Controller
         $message = $request->input('message');
         $subject = $request->input('subject');
         if (!$request->input('name') || $name === '') {
-            print json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
+            echo json_encode(array('message' => 'Name cannot be empty', 'code' => 0));
             exit();
         }
         if (!$request->input('email') || $email === '') {
-            print json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
+            echo json_encode(array('message' => 'Email cannot be empty', 'code' => 0));
             exit();
         } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            print json_encode(array('message' => 'Email format invalid', 'code' => 0));
+            echo json_encode(array('message' => 'Email format invalid', 'code' => 0));
             exit();
         }
         if (!$request->input('subject') || $subject === '') {
-            print json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
+            echo json_encode(array('message' => 'Subject cannot be empty', 'code' => 0));
             exit();
         }
         if (!$request->input('message') || $message === '') {
-            print json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
+            echo json_encode(array('message' => 'Message cannot be empty', 'code' => 0));
             exit();
         }
         $content="From: $name \nEmail: $email \nMessage: $message";
@@ -48,22 +48,22 @@ class MailController extends Controller
         $message = $request->input('message');
         $subject = $request->input('subject');
         if (!$request->input('name') || $name === '') {
-            print json_encode(array('message' => 'Họ tên không được để trống', 'code' => 0));
+            echo json_encode(array('message' => 'Họ tên không được để trống', 'code' => 0));
             exit();
         }
         if (!$request->input('email') || $email === '') {
-            print json_encode(array('message' => 'Email không được để trống', 'code' => 0));
+            echo json_encode(array('message' => 'Email không được để trống', 'code' => 0));
             exit();
         } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            print json_encode(array('message' => 'Định dạng email sai', 'code' => 0));
+            echo json_encode(array('message' => 'Định dạng email sai', 'code' => 0));
             exit();
         }
         if (!$request->input('subject') || $subject === '') {
-            print json_encode(array('message' => 'Chủ đề không được để trống', 'code' => 0));
+            echo json_encode(array('message' => 'Chủ đề không được để trống', 'code' => 0));
             exit();
         }
         if (!$request->input('message') || $message === '') {
-            print json_encode(array('message' => 'Tin nhắn không được để trống', 'code' => 0));
+            echo json_encode(array('message' => 'Tin nhắn không được để trống', 'code' => 0));
             exit();
         }
         $content="<p>Từ: $name</p><p>Email: $email</p><p>Tin nhắn: $message</p>";
